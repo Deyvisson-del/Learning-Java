7de72edc7febbd65b5eb7db6a571e33b1ebb8f02
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-02-24 17:26:40 -0300

    Refactor App class and add Client class

---
+++ src/edu/deyvisson/first_week/Client.java
@@ -0,0 +1,8 @@
+package edu.deyvisson.first_week;
+
+
+public class Client {
+    private String name, email, phone, nameFather, nameMother, address;
+    private int idCode, age, money, idClient;
+    private boolean active;
+}
